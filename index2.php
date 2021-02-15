a9a459fd0f1318b3b56a696ca9b250e1e82c4d9c
EpicModoDios <tomyperez403@gmail.com>
2021-02-15 17:11:56 -0500

    Variables, Tipos De Datos y Constantes 15-02-21

---
+++ index2.php
@@ -0,0 +1,14 @@
+
+
+<?php
+
+//Definir una constante
+define("numero", 3.1415);
+echo numero;
+
+echo "<br>";
+
+const letras = "Thomas";
+echo letras;
+
+?>
---
+++ index3.php
@@ -0,0 +1,24 @@
+
+
+<?php
+//Remplazo de variables
+    $letras = (string) "Thomas Perez";
+    echo $letras;
+
+    echo "<br>";
+
+    $letras = (string) "Isabella";
+    echo $letras;
+
+    echo "<br>";
+
+    //Las constantes no pueden ser remplazadas
+    const numero = 30;
+    echo numero;
+
+    echo "<br>";
+
+    const Numero = 50;
+    echo Numero;
+
+?>
